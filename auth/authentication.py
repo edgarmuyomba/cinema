45b7e49d534c5d7220aad28db9c1af7d4cc856d5
edgarmuyomba <edgarmatthew247@gmail.com>
2024-07-20 09:26:09 +0300

    refactoring authenticate class

--- auth/authentication.py
+++ auth/authentication.py
@@ -2,7 +2,7 @@ from cryptography.fernet import Fernet
 import os
 import requests
 from getpass import getpass
-from ..Formatter import Formatter
+from Formatter import Formatter
 import json
 
 base_url = "http://localhost:8000"
@@ -10,12 +10,12 @@ base_url = "http://localhost:8000"
 class Authentication:
     def __init__(self):
         self.key = None
-        if os.path.exists('secret.key'):
-            with open('secret.key', 'rb') as key_file:
+        if os.path.exists('auth/secret.key'):
+            with open('auth/secret.key', 'rb') as key_file:
                 self.key = key_file.read()
         else:
             self.key = Fernet.generate_key()
-            with open('secret.key', 'wb') as key_file:
+            with open('auth/secret.key', 'wb') as key_file:
                 key_file.write(self.key)
 
         self.cipher_suite = Fernet(self.key)
@@ -24,17 +24,17 @@ class Authentication:
     def store_token(self, token: str):
         token_bytes = token.encode('utf-8')
         encrypted_token = self.cipher_suite.encrypt(token_bytes)
-        with open('token.enc', 'wb') as token_file:
+        with open('auth/token.enc', 'wb') as token_file:
             token_file.write(encrypted_token)
     
     def get_token(self):
-        if os.path.exists('token.enc'):
-            with open('token.enc', 'rb') as token_file:
+        if os.path.exists('auth/token.enc'):
+            with open('auth/token.enc', 'rb') as token_file:
                 encrypted_token = token_file.read()
                 decrypted_token = self.cipher_suite.decrypt(encrypted_token).decode()
                 return decrypted_token
         else:
-            with open('config.json', 'r') as config_file:
+            with open('auth/config.json', 'r') as config_file:
                 config = json.load(config_file)
                 if config['hasAccount']:
                     token = self.login()
@@ -42,7 +42,7 @@ class Authentication:
                     token = self.signup()
             if token:
                 self.store_token(token)
-                with open('config.json', 'w') as  config_file:
+                with open('auth/config.json', 'w') as  config_file:
                     config = json.dumps({'hasAccount': True})
                     config_file.write(config)
                 return token
@@ -70,12 +70,18 @@ class Authentication:
             elif response.status_code == 400:
                 errors = response.json()
                 self.formatter.format_rule("Errors")
-                for key, value in errors.enumerate:
-                    for error in value:
+                for values in errors.values():
+                    for error in values:
                         self.formatter.format_error(error)
                         if error == "Unable to log in with provided credentials.":
-                            token = self.signup()
-                            break             
+                            print()
+                            user_response = input("Do you have an account? (Y/N)\n")
+
+                            if user_response == "Y":
+                                continue 
+                            else:
+                                token = self.signup()
+                                break             
                 continue
 
         if not token:
@@ -99,14 +105,14 @@ class Authentication:
 
             response = requests.post(f'{base_url}/accounts/signup/', json=body)
 
-            if response.status_code == 200:
+            if response.status_code == 201:
                 token = response.json()['token']
                 break
             elif response.status_code == 400:
                 errors = response.json()
                 self.formatter.format_rule("Errors")
-                for key, value in errors.enumerate:
-                    for error in value:
+                for values in errors.values():
+                    for error in values:
                         self.formatter.format_error(error)
                 continue
             else:
